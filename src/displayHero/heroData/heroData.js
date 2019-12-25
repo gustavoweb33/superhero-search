eebe26d0c473fa15e106c09ae41be92da14b2e66
gustavoweb33 <lealgustavo33@gmail.com>
2019-12-24 22:52:27 -0600

    created an array to filter through hero aliases

--- src/displayHero/heroData/heroData.js
+++ src/displayHero/heroData/heroData.js
@@ -4,24 +4,40 @@ import style from './heroData.module.css'
 class HeroData extends Component {
     render() {
         const heroDataProps = { ...this.props.stats }
+
+        let aliases = [];
+        //check if props is the 'aliase' key in the object
+        if ( heroDataProps.aliases ) {
+            /*make a separate array otherwise when it prints on the screen, it will be one long line instead of seperate entries */
+            aliases = [ ...heroDataProps.aliases ];
+        }
+
         return (
-          
-                <div className={ style.border }>
 
-                    { Object.keys( heroDataProps ).map( ( key ) => {
+            <div className={ style.border }>
 
-                        return <div key={ key }>
-                            <span
-                                style={ { fontWeight: 'bold', textTransform: 'uppercase' } }>
-                                { key }
-                            </span>
-                            : { heroDataProps[ key ] }
-                        </div>
+                { Object.keys( heroDataProps ).map( ( key ) => {
+                    return <div key={ key }>
+                        <span
+                            style={ { fontWeight: 'bold', textTransform: 'uppercase' } }>
+                            { key }
+                        </span>
+                        :
+                        <span>
+                            { heroDataProps[ key ] }
+                        </span>
+                    </div>
 
-                    } ) }
+                } ) }
 
-                </div>
-           
+                <span>ALIASES</span>
+                { aliases.map( ( aliase ) => {
+                    return <div key={ aliase }>
+                        { aliase }
+                    </div>
+                } ) }
+            </div>
+
         )
     }
 }
