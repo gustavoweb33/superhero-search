9ffb613c97b9c13e2a6d432e85dcf008042e3611
gustavoweb33 <lealgustavo33@gmail.com>
2020-01-08 19:01:55 -0600

    created new component to show error message when hero is not found

---
+++ src/displayHero/heroData/heroNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const heroNotFound = () => {
+    return (
+        <div>
+            <h1>No Hero Found</h1>
+            <p>Try Again</p>
+        </div>
+    )
+}
+
+export default heroNotFound;
