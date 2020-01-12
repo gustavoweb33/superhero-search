6d0498418b6771b84076ef40a3c401eebe124b93
gustavoweb33 <lealgustavo33@gmail.com>
2020-01-11 19:20:53 -0600

    added examples to the error displayed

--- src/displayHero/heroData/heroNotFound.js
+++ src/displayHero/heroData/heroNotFound.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import NotFoundIcon from '../../icons/sad-1.png'
+import style from './heroNotFound.module.css'
 
 const heroNotFound = () => {
     return (
-        <div>
+        <div className={style.container}>
             <h1>No Hero Found</h1>
-            <p>Try Again</p>
+            <p>Check your spelling. Heroes with the same name have Roman numbers after their name</p>
+            <p>Exaple of heros, Anti-Venom, Batgirl II</p>
+            <p>If no matching hero is found. Try the heros real full name</p>
+            <img src={ NotFoundIcon } alt='sad face'/>
         </div>
-    )
-}
-
+            )
+        }
+        
 export default heroNotFound;
