338527db5458fb0857b849148c1fcca2c6eeaabe
gustavoweb33 <lealgustavo33@gmail.com>
2020-01-11 19:18:20 -0600

    crediting the creators of the icons used

---
+++ src/iconCredit/iconCredit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IconsCredit = () => {
+    return (
+        <div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik
+                </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            </div>
+        </div>
+    )
+}
+
+export default IconsCredit;
