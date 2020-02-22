d4f4e77482e53dd73f3d3e90bd2d4fa4f8b77c29
gustavoweb33 <lealgustavo33@gmail.com>
2020-02-22 17:51:14 -0600

    Shortend message and fixes spelling

--- src/displayHero/heroData/heroNotFound.js
+++ src/displayHero/heroData/heroNotFound.js
@@ -4,14 +4,14 @@ import style from './heroNotFound.module.css'
 
 const heroNotFound = () => {
     return (
-        <div className={style.container}>
+        <div className={ style.container }>
             <h1>No Hero Found</h1>
-            <p>Check your spelling. Heroes with the same name have Roman numbers after their name</p>
-            <p>Exaple of heros, Anti-Venom, Batgirl II</p>
-            <p>If no matching hero is found. Try the heros real full name</p>
-            <img src={ NotFoundIcon } alt='sad face' className={style.notFoundFace}/>
+            <p>Check your spelling. Some heroes might have a dash or Roman number in their name.</p>
+            <p>Exaple: Anti-Venom, Batgirl II.</p>
+            <p>If no matching hero is found. Try the hero's real full name.</p>
+            <img src={ NotFoundIcon } alt='sad face' className={ style.notFoundFace } />
         </div>
-            )
-        }
-        
+    )
+}
+
 export default heroNotFound;
